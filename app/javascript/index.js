493f8a6911b64188d45921d248555d60a8fa7633
Brett Tanner <bretttanner171@gmail.com>
2022-10-11 10:51:23 +0900

    Add touch event listener to index view

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -3,3 +3,8 @@ articles.forEach(article =>
     article.addEventListener('click', function() {
         this.classList.toggle('active_article')
     }));
+
+articles.forEach(article => 
+    article.addEventListener('touch', function() {
+        this.classList.toggle('active_article')
+    }));
